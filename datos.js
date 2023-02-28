cc7757ffc707c7a0f72579cae15a2a99fc63cc8b
Lex-Luthor7 <124512670+Lex-Luthor7@users.noreply.github.com>
2023-02-28 20:27:01 -0300

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,10 +1,10 @@
 
 function myfunction(){
     var x = document.getElementById("ocultar");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    if (x.style.display === "none") {
+        x.style.display = "inline";
     }
     else {
-        x.style.display = "block";
+        x.style.display = "none";
     }
     }
