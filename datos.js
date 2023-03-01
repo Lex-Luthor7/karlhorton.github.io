5a434908c4565e32e1b29a44588878aa85481f04
Lex-Luthor7 <124512670+Lex-Luthor7@users.noreply.github.com>
2023-03-01 10:49:42 -0300

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,27 @@
+
+function myfunction(){
+    var x = document.getElementById("ocultar");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+    }
+
+/*
+function ocultar(){
+    document.getElementById ("col-1").style.display = "block";
+}
+*/
+
+
+function myFunction(){
+    var x = document.getElementById("ocultar");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+    }
