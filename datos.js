387a111d442472b6062fd1f510d23813f2256bbd
Lex-Luthor7 <124512670+Lex-Luthor7@users.noreply.github.com>
2023-03-01 14:26:32 -0300

    Update datos.js

--- datos.js
+++ datos.js
@@ -8,20 +8,3 @@ function myfunction(){
         x.style.display = "block";
     }
     }
-
-/*
-function ocultar(){
-    document.getElementById ("col-1").style.display = "block";
-}
-*/
-
-
-function myFunction(){
-    var x = document.getElementById("ocultar");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    }
-    else {
-        x.style.display = "block";
-    }
-    }
